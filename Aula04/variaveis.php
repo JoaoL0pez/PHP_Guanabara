a5ba18944341e6a8e29bff51deefca60b387b97a
João Lopes <contact.lopesdesign@gmail.com>
2023-01-16 21:04:04 -0300

    Adição da atividade da aula 04

---
+++ Aula04/variaveis.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Olá, Mundo!</title>
+    <style>
+        h2 {
+            color: blue;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP </h1>
+    <?php
+        $nome = "João";
+        $idade = "21";
+        
+        echo $nome." tem ".$idade." anos! ";
+    ?>
+    
+</body>
+</html>
