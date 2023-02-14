3a96fd5d700e79720c6c336c83aed0af5da22575
João Lopes <contact.lopesdesign@gmail.com>
2023-02-13 23:47:12 -0300

    adição novas aulas

---
+++ PHP/Aula15/02-exercicio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="../_css.css"/>
+    <meta charset="UTF-8">
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+    <?php
+        include "funcoes.php";
+        print "<h1> Testando novas funcoes </h1";
+        ola();
+        mostraValor(4);
+        print "<h2>Finalizando programa...</h2>";
+
+
+        // require - ele procurará o arquivo inserido, porém se não encontrar, ele vai parar o processo.
+        // include - procurará o arquivo, porém se não encontrar, irá continuar o processo / function.
+    ?>
+    </br><a href="">Voltar</a>
+</body>
+</html>
